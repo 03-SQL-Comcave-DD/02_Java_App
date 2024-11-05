2bc489a2dabb37982fea7996a2c9febbf2924d45
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-11-05 11:32:05 +0000

    employees anzeigen lassen und rs/stmt/con wieder schließen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,7 +1,12 @@
 package com.cc.java;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
+
+// Kann man auch so schreiben
+// import java.sql.*
 
 public class App 
 {
@@ -11,22 +16,59 @@ public class App
     static String usrPwd = "pass@remote";
         
     static Connection con;
-    //static Statement stmt;
+    static Statement stmt;
     static ResultSet rs;
 
     public static void main(String[] args) {
 
     try {
             
-        // Load SQL Server JDBC driver and establish connection.
-        System.out.print("Verbindung wird aufgebaut ... ");
+// Load SQL Server JDBC driver and establish connection.
+    System.out.print("Verbindung wird aufgebaut ... ");
+
+    try (Connection con = DriverManager.getConnection(conURL, usrStr, usrPwd)) {
+      
+        System.out.println("Verbindung erfolgreich hergestellt!");
+
+ // Creating a statement object
+        Statement stmt = con.createStatement();
+
+ // Executing the query and storing the result in a ResultSet object
+        ResultSet rs = stmt.executeQuery("SELECT * FROM mydb.employees");
 
 
+ // Iterating through the ResultSet and printing the contents
+ while (rs.next()) {
 
-        } catch (Exception e) {
+    // fetch
+    String name = rs.getString("name");
+    String surname = rs.getString("surname");
+    int age = rs.getInt("age");
+    int salary = rs.getInt("salary");
+    int employee_id = rs.getInt("employee_id");
 
-            // TODO: handle exception
-        }
+    // print
+    String printStr =   "ID: " + employee_id +
+                        ", Name: " + surname + " " + name +
+                        ", Salary: " + salary +
+                        ", Age: " + age;
+    System.out.println(printStr);
+}
+
+rs.close();
+stmt.close();
+con.close();
+
+    } catch (Exception e) {
+        e.printStackTrace();  
+    }
+
+    
+
+    }   catch (Exception e) {
+        e.printStackTrace();
+    
+    }
 
 
 
