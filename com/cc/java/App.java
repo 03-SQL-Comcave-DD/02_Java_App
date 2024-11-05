ac02b1f5641568210734e99298e25c9a7fa63407
Rene-GitHub- <168723204+Ichnixdu@users.noreply.github.com>
2024-11-05 10:01:53 +0000

    testen ob diese Methode funktioniert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,10 +1,35 @@
 package com.cc.java;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 public class App 
 {
+ // Argumente für Connection-Parameter
+    static String conURL = "jdbc:mysql://172.104.240.94:3306/mydb";
+    static String usrStr = "remote_user";
+    static String usrPwd = "pass@remote";
+        
+    static Connection con;
+    //static Statement stmt;
+    static ResultSet rs;
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    try {
+            
+        // Load SQL Server JDBC driver and establish connection.
+        System.out.print("Verbindung wird aufgebaut ... ");
+
+
+
+        } catch (Exception e) {
+
+            // TODO: handle exception
+        }
+
+
+
     }
     
 }
